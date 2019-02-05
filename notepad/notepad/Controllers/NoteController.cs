613c32c379827117920451520781a272c5091ba3
nataszaplachta <natasza.plachta@gmail.com>
2019-02-05 19:13:02 +0100

    creating model and controller

---
+++ notepad/notepad/Controllers/NoteController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using notepad.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace notepad.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : Controller
+    {
+        private readonly NoteContext _context;
+
+        public NoteController(NoteContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.TodoItems.Add(new Note{ Title = "Note1" });
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ notepad/notepad/Models/NoteContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace notepad.Models
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Note> TodoItems { get; set; }
+    }
+}
